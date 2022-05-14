cf3395460572375f46f3a0517a8c906e52ed1fc5
Adrián Marquina <adrimarq2000@gmail.com>
2022-05-14 11:38:43 +0200

    Cost implementation dev sepparated

--- parametrosIteracion.m
+++
@@ -1,6 +0,0 @@
-costePaneles=precioW*PgBasemax/periodoVidaPan;
-costeBateria=precioWh*capacidadMax/periodoVidaBat;
-litrosCombustible=ENS/(consumoGrupo/1000);
-costeCombustible=precioCombustible*litrosCombustible;
-costeTotal[sistema] = costePaneles + costeBateria + costeCombustible;
-fiabilidad[sistema] = 100-LOLP;
